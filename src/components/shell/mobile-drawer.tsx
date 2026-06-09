f64529a0aab595aa3c0c973ce80e9171bd364452
乙多れ个Ð Kれ多ภ <zahidkhan007860@gmail.com>
2026-06-09 09:52:34 +0530

    fix ui issue
    fix ui issue padding

--- src/components/shell/mobile-drawer.tsx
+++ src/components/shell/mobile-drawer.tsx
@@ -17,12 +17,12 @@ export function MobileDrawer({ isAdmin = false }: { isAdmin?: boolean }) {
           <Menu className="h-5 w-5" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="left-0 top-0 h-dvh w-[18rem] max-w-[80vw] translate-x-0 translate-y-0 rounded-none border-l-0 p-0 [&>button[aria-label='Close']]:right-3 [&>button[aria-label='Close']]:top-3">
+      <DialogContent className="left-0 top-0 h-dvh w-[20rem] max-w-[80vw] translate-x-0 translate-y-0 rounded-none border-l-0 p-0 [&>button[aria-label='Close']]:right-3 [&>button[aria-label='Close']]:top-3">
         <div className="flex h-full flex-col">
           <DialogHeader className="border-b border-border p-5">
             <DialogTitle>Menu</DialogTitle>
           </DialogHeader>
-          <div className="flex-1 p-3" onClick={() => setOpen(false)}>
+          <div className="flex-1 p-4" onClick={() => setOpen(false)}>
             <SidebarNav isAdmin={isAdmin} />
           </div>
         </div>
